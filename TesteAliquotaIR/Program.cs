4d2ad3140c52eb677e86cdaa8962fe617e49c8c9
Bruno Lazzara Queiroz <34097455+bruno-lazzara@users.noreply.github.com>
2022-02-09 21:12:28 -0300

    Add files via upload

---
+++ TesteAliquotaIR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteAliquotaIR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = 3300.0;
+
+            if (salario >= 1900.0 && salario <= 2800.0)
+            {
+                Console.WriteLine("O IR é de 7.5%");
+                Console.WriteLine("Pode deduzir na declaração o valor de R$ 142,00");
+            }
+            else if (salario >= 2800.01 && salario <= 3751.0)
+            {
+                Console.WriteLine("O IR é de 15%");
+                Console.WriteLine("Pode deduzir na declaração o valor de R$ 350,00");
+            }
+            else if (salario >= 3751.01 && salario <= 4664.0)
+            {
+                Console.WriteLine("O IR é de 22.5%");
+                Console.WriteLine("Pode deduzir na declaração o valor de R$ 636,00");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
